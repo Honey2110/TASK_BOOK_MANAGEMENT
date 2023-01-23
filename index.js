55ee43d073afdb16db21a720cc47610699eb2f1f
Honey2110 <swapnil21.sk@gmail.com>
2023-01-24 01:01:20 +0530

    Learned about routes & completed users parts

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const express = require ("express");
+const express = require("express");
+
+const { users } = require("./data/users.json");
 
 const app = express();
 
@@ -12,6 +14,68 @@ app.get("/", (req, res) => {
     }); 
 });
 
+/** */
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        "success": "true",
+        "data": users,
+    });
+});
+
+
+app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success : false,
+            message: "Not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        message: user,
+    });
+});
+
+app.post("/users", (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
+    const user = users.find((each) => each.id === id);
+    
+    if (user) {
+        res.status(404).json({
+            success: false,
+            message: "User already exist"
+        });
+    }
+    users.push({
+        id, name, surname, email, subscriptionType, subscriptionDate
+    });
+    return res.status(201).json({
+        success: true,
+        message: users
+    });  
+});
+
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+
+    const user = users.find((each) => each.id === id);
+    if (!user) return res.status(404).json({ success: false, message: "User not found" });
+    const updatedUser = users.map((each) => {
+        if (each.id === id) {
+            return {
+                ...each,
+                ...data,
+            }
+        }
+        return each;
+    });
+    return res.status(200).json({success:true , data : updatedUser})
+});
+
 app.get("*", (req, res) => {
     res.status(404).json({
        "message":"YEH kaha Aa gye hum"
@@ -20,4 +84,4 @@ app.get("*", (req, res) => {
 
 app.listen(port,(req,res)=>{
     console.log(`Server is Running at port ${port}`);
-});
+}); 
