f4d0d41e59b3403ec1c1adf24ee715062f02ad3a
Honey2110 <swapnil21.sk@gmail.com>
2023-01-24 21:46:24 +0530

    COMPLETED books section

--- index.js
+++ index.js
@@ -1,87 +1,29 @@
 const express = require("express");
-
-const { users } = require("./data/users.json");
+// importing routes
+const usersRouter = require("./routes/users");
+const booksRouter = require("./routes/books");
 
 const app = express();
 
-const port = 8081;
+const PORT = 8081;
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.status(200).json({
-    "message" : "Server is running fine"
-    }); 
+  res.status(200).json({
+    message: "Server is up and running",
+  });
 });
 
-/** */
-
-app.get("/users", (req, res) => {
-    res.status(200).json({
-        "success": "true",
-        "data": users,
-    });
-});
-
-
-app.get("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const user = users.find((each) => each.id === id);
-    if (!user) {
-        return res.status(404).json({
-            success : false,
-            message: "Not found",
-        });
-    }
-    return res.status(200).json({
-        success: true,
-        message: user,
-    });
-});
-
-app.post("/users", (req, res) => {
-    const { id, name, surname, email, subscriptionType, subscriptionDate } = req.body;
-    const user = users.find((each) => each.id === id);
-    
-    if (user) {
-        res.status(404).json({
-            success: false,
-            message: "User already exist"
-        });
-    }
-    users.push({
-        id, name, surname, email, subscriptionType, subscriptionDate
-    });
-    return res.status(201).json({
-        success: true,
-        message: users
-    });  
-});
-
-app.put("/users/:id", (req, res) => {
-    const { id } = req.params;
-    const { data } = req.body;
-
-    const user = users.find((each) => each.id === id);
-    if (!user) return res.status(404).json({ success: false, message: "User not found" });
-    const updatedUser = users.map((each) => {
-        if (each.id === id) {
-            return {
-                ...each,
-                ...data,
-            }
-        }
-        return each;
-    });
-    return res.status(200).json({success:true , data : updatedUser})
-});
+app.use("/users", usersRouter);
+app.use("/books", booksRouter);
 
 app.get("*", (req, res) => {
-    res.status(404).json({
-       "message":"YEH kaha Aa gye hum"
-    }); 
+  res.status(404).json({
+    message: "This route does not exist",
+  });
 });
 
-app.listen(port,(req,res)=>{
-    console.log(`Server is Running at port ${port}`);
-}); 
+app.listen(PORT, () => {
+  console.log(`Server is running at port ${PORT}`);
+});
